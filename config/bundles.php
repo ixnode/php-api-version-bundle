9751a0ce8f1d7e0b61e83ba9f556b8185a883f94
Björn Hempel <bjoern@hempel.li>
2023-06-25 10:47:12 +0200

    Add symfony profiler

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
